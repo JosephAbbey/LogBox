913e3926490fb8904efe422e082793337484196c
Joseph Abbey <joseph@abbey1.org.uk>
2022-07-10 12:49:03 +0100

    better navbar loading

--- app/core/components/NavBar.tsx
+++ app/core/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState, MouseEvent } from 'react';
+import { Suspense, useState, MouseEvent, useRef, RefObject } from 'react';
 import {
     IconButton,
     Avatar,
@@ -10,14 +10,16 @@ import {
     Tooltip,
     Paper,
     Badge,
+    Box,
+    Portal,
+    CircularProgress,
 } from '@mui/material';
 import { useCurrentUser } from 'app/core/hooks/useCurrentUser';
 import logout from 'app/auth/mutations/logout';
 import { useMutation, Routes, Link } from 'blitz';
-import * as React from 'react';
 import { Home, Logout, Settings } from '@mui/icons-material';
 
-function NavBarInner() {
+function Profile({ root }: { root: RefObject<HTMLDivElement> }) {
     const currentUser = useCurrentUser();
     const [logoutMutation] = useMutation(logout);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -31,6 +33,102 @@ function NavBarInner() {
 
     return (
         <>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2, marginLeft: 'auto', marginRight: '.5em' }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <Badge badgeContent={currentUser ? undefined : '?'} color="primary">
+                        <Avatar sx={{ width: 32, height: 32 }}>
+                            <Settings />
+                        </Avatar>
+                    </Badge>
+                </IconButton>
+            </Tooltip>
+            <Portal container={root.current}>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    {currentUser
+                        ? [
+                              <MenuItem key={0}>
+                                  User id:&nbsp;<code>{currentUser.id}</code>
+                              </MenuItem>,
+                              <MenuItem key={1}>
+                                  User role:&nbsp;<code>{currentUser.role}</code>
+                              </MenuItem>,
+                              <Divider key={2} />,
+                              <MenuItem
+                                  key={3}
+                                  onClick={async () => {
+                                      await logoutMutation();
+                                  }}
+                              >
+                                  <ListItemIcon>
+                                      <Logout fontSize="small" />
+                                  </ListItemIcon>
+                                  Logout
+                              </MenuItem>,
+                          ]
+                        : [
+                              <MenuItem key={0}>
+                                  <Link href={Routes.SignupPage()}>
+                                      <strong>Sign Up</strong>
+                                  </Link>
+                              </MenuItem>,
+                              <MenuItem key={1}>
+                                  <Link href={Routes.LoginPage()}>
+                                      <strong>Login</strong>
+                                  </Link>
+                              </MenuItem>,
+                          ]}
+                </Menu>
+            </Portal>
+        </>
+    );
+}
+
+function NavBarInner() {
+    const root = useRef<HTMLDivElement>(null);
+
+    return (
+        <Box ref={root}>
             <Paper
                 sx={{
                     display: 'flex',
@@ -46,7 +144,15 @@ function NavBarInner() {
             >
                 <Link href={Routes.Home()}>
                     <IconButton size="small" sx={{ ml: 2, marginX: '.5em' }}>
-                        <Avatar sx={{ width: 32, height: 32 }}>
+                        <Avatar
+                            sx={{
+                                width: 32,
+                                height: 32,
+                                ml: 2,
+                                marginLeft: 'auto',
+                                marginRight: '.5em',
+                            }}
+                        >
                             <Home />
                         </Avatar>
                     </IconButton>
@@ -56,93 +162,22 @@ function NavBarInner() {
                     <Typography sx={{ minWidth: 100, cursor: 'pointer' }}>My Apps</Typography>
                 </Link>
 
-                <Tooltip title="Account settings">
-                    <IconButton
-                        onClick={handleClick}
-                        size="small"
-                        sx={{ ml: 2, marginLeft: 'auto', marginRight: '.5em' }}
-                        aria-controls={open ? 'account-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                    >
-                        <Badge badgeContent={currentUser ? undefined : '?'} color="primary">
+                <Suspense
+                    fallback={
+                        <IconButton
+                            size="small"
+                            sx={{ ml: 2, marginLeft: 'auto', marginRight: '.5em' }}
+                        >
                             <Avatar sx={{ width: 32, height: 32 }}>
-                                <Settings />
+                                <CircularProgress size="1em" color="inherit" />
                             </Avatar>
-                        </Badge>
-                    </IconButton>
-                </Tooltip>
+                        </IconButton>
+                    }
+                >
+                    <Profile root={root} />
+                </Suspense>
             </Paper>
-            <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        '& .MuiAvatar-root': {
-                            width: 32,
-                            height: 32,
-                            ml: -0.5,
-                            mr: 1,
-                        },
-                        '&:before': {
-                            content: '""',
-                            display: 'block',
-                            position: 'absolute',
-                            top: 0,
-                            right: 14,
-                            width: 10,
-                            height: 10,
-                            bgcolor: 'background.paper',
-                            transform: 'translateY(-50%) rotate(45deg)',
-                            zIndex: 0,
-                        },
-                    },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-            >
-                {currentUser
-                    ? [
-                          <MenuItem key={0}>
-                              User id:&nbsp;<code>{currentUser.id}</code>
-                          </MenuItem>,
-                          <MenuItem key={1}>
-                              User role:&nbsp;<code>{currentUser.role}</code>
-                          </MenuItem>,
-                          <Divider key={2} />,
-                          <MenuItem
-                              key={3}
-                              onClick={async () => {
-                                  await logoutMutation();
-                              }}
-                          >
-                              <ListItemIcon>
-                                  <Logout fontSize="small" />
-                              </ListItemIcon>
-                              Logout
-                          </MenuItem>,
-                      ]
-                    : [
-                          <MenuItem key={0}>
-                              <Link href={Routes.SignupPage()}>
-                                  <strong>Sign Up</strong>
-                              </Link>
-                          </MenuItem>,
-                          <MenuItem key={1}>
-                              <Link href={Routes.LoginPage()}>
-                                  <strong>Login</strong>
-                              </Link>
-                          </MenuItem>,
-                      ]}
-            </Menu>
-        </>
+        </Box>
     );
 }
 
